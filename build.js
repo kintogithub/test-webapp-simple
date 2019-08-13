8393c922427b40a21da948290f9898eeb858b31c
kintogithub <50351958+kintogithub@users.noreply.github.com>
2019-08-13 17:52:49 +0530

    Update build.js

--- build.js
+++ build.js
@@ -4,6 +4,6 @@ const fs = require('fs');
 mkdirp('./public')
 
 let content = fs.readFileSync('web/index.html').toString();
-content = content.replace('Anonymous', process.env.USERNAME || 'Nobody')
+content = content.replace('Anonymous', process.env.USERNAME && process.env.USERNAMES || 'Nobody')
 
 fs.writeFileSync('public/index.html', content);
